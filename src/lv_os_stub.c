f77e0fde21dd5d7f232b896190d11485afb12b27
GeeMing <geeming@foxmail.com>
2026-01-04 15:43:40 +0800

    rm: 删掉无用组件

--- src/lv_os_stub.c
+++
@@ -1,8 +0,0 @@
-/* Minimal OS stub to provide lv_os_get_idle_percent when no RTOS is used */
-#include <stdint.h>
-
-uint32_t lv_os_get_idle_percent(void)
-{
-    /* No RTOS idle tracking on this port. Return 0% idle. */
-    return 0;
-}
